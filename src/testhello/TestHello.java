0c09490041c90bbdca37e15dae4b57751a5682f5
KrengsakGit <pondnakub08.gmail.com>
2022-03-25 14:32:37 +0700

    second commit

--- src/testhello/TestHello.java
+++ src/testhello/TestHello.java
@@ -16,6 +16,7 @@ public class TestHello {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
+        System.out.println("I L U");
     }
     
 }
